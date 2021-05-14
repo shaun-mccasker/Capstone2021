ba2deda166ca0c87933964de5221a22348064740
shaun-mccasker <shaun.mccasker1999@gmail.com>
2021-05-14 13:33:40 +1000

    updated variable names

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,13 @@ export default function routes(app, addon) {
 
     // // This is an example route used by "generalPages" module (see atlassian-connect.json).
     // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
+    app.get('/agile-roadmap', addon.authenticate(), (req, res) => {
         // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
         // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
         res.render(
-          'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+          'agile-roadmap.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
           {
-            title: 'Agile roadmap hdiushaiudhsauihdk'
+            title: 'Agile roadmap'
             //, issueId: req.query['issueId']
             //, browserOnly: true // you can set this to disable server-side rendering for react views
           }
